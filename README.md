c47aeba7c6b069d713877de937aec72d83fbca2d
gabriel <gabriel.cardoso010@gmail.com>
2017-12-15 14:02:28 -0200

    Regras de postagem de comentario

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # api-comentarios
+
+Projeto desenvolvido para a realização de um desafio.
+
+### Prerequisites
+
+```
+PHP7
+COMPOSER
+MYSQL
+APACHE2 OR NGINX
+```
+
+## Getting Started
+
+```
+git clone https://github.com/cardoso010/api-comentarios
+cd api-comentarios/
+sudo composer update
+```
+
+
+## Authors
+
+* **Gabriel Cardoso Luiz** - *Initial work* - [cardoso010](https://github.com/cardoso010)
+
+
+
+
--- app/Services/ComentarioService.php
+++ app/Services/ComentarioService.php
@@ -10,10 +10,13 @@ use Validator;
 use Illuminate\Http\Request;
 use App\Services\PostagemService;
 use App\Exceptions\GenericException;
+use DB;
 
 class ComentarioService
 {
+    // constante que indica o maximo de comentarios em segundos
     const MAXCOMENTARIOS = 5;
+    // constante que indica os segundos
     const SEGUNDOSCOMENTARIOS = 10;
 
     public function __construct(Comentario $comentario){
@@ -24,48 +27,37 @@ class ComentarioService
      * Metodo responsavel por validar inserção do comentario
      */
     public function validar(Request $request){
-        $retorno = false;
-
         $usuarioComentario = Usuario::findOrFail($request->get('usuario_id'));
         $this->validarLimiteComentarioSegundo($usuarioComentario->id);
-        error_log($usuarioComentario->assinante);
-        if($usuarioComentario->assinante){
-            error_log("entrou primeiro if");
-            $retorno = true;
-        } else {
-            error_log("entrou else");
+        if(!$usuarioComentario->assinante){
             $usuarioPost = (new PostagemService(new Postagem()))->getUsuarioByPost($request->get('postagem_id'));
             if(!$usuarioComentario->assinante){
-                error_log("entrou if que não é assinante");
                 $comprandoDestaque = $request->get('comprando_destaque');
-                if(!empty($comprandoDestaque) && $comprandoDestaque){
-                    $retorno = true;
-                    error_log("entrou if que pode");
-                } else {
-                    error_log("entrou else que não pode inserir");
+                if(empty($comprandoDestaque) && !$comprandoDestaque){
                     throw new GenericException("Usuario não pode inserir comentario pois não é assinante e não está comprando destaque"); 
-                }
+                } 
             }
-        }
-
-        return $retorno;
+        } 
     }
 
     /**
      * Metodo responsavel por validar limite de inserção de comentarios por segundos
      */
     public function validarLimiteComentarioSegundo($usuario_id){
-        $quantidade = $this->comentario
-                            ->where('usuario_id', $usuario_id)
-                            ->whereBetween('created_at', [
-                                                            \Carbon\Carbon::now(),
-                                                            \Carbon\Carbon::now()->subSeconds(ComentarioService::SEGUNDOSCOMENTARIOS)
-                                                         ]
-                            )->count();
-        if($quantidade > ComentarioService::MAXCOMENTARIOS){
-            throw new GenericException("Não se pode fazer mais que 5 comentarios em 10 segundos"); 
-        }
+        $quantidade = DB::table('comentarios')
+                     ->select(DB::raw('count(*) as quantidade'))
+                     ->where('usuario_id', '=', $usuario_id)
+                     ->whereBetween('created_at', [
+                                        \Carbon\Carbon::now()->subSeconds(ComentarioService::SEGUNDOSCOMENTARIOS)->toDateTimeString(),
+                                        \Carbon\Carbon::now()->toDateTimeString()
+                                        ]
+                                    )
+                     ->get();
 
+        if(intval($quantidade[0]->quantidade) > ComentarioService::MAXCOMENTARIOS){
+            error_log("Entrou");
+            throw new GenericException("Não é permitido inserir mais que 5 comentarios em 10 segundos"); 
+        }
     }
 
     /**
